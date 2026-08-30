66ec13bc1cb780dab88ec104371d464de55bc5d9
Natnael Berhanu <natnaelberhanumuluneh@gmail.com>
2026-08-31 01:33:25 +0300

    second  commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
     stopAutoSlide();
     slideInterval = setInterval(() => {
       goToSlide(currentSlideIndex + 1);
-    }, 6500);
+    }, 5500);
   }
 
   function stopAutoSlide() {
